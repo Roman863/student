6fcf6d67c0b077d672ff79a70f2ea7286337315e
Roman863 <r.izvekov1998@gmail.com>
2019-10-16 10:34:39 +0700

    добавлен класс Student

---
+++ student.h
@@ -0,0 +1,14 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+#include "human.h"
+class Student : public Human {
+public:
+        Student ();
+	Student(char*, int,int char*);
+	void say();
+	char *group;
+};
+
+
+#endif
+
